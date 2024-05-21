a1037a633da8f1cb927677ddc80d3bc4f3391a45
NaZhang <32621160+nz0001na@users.noreply.github.com>
2024-05-20 20:06:20 -0700

    Add files via upload

--- 46_permutation.py
+++ 46_permutation.py
@@ -1,56 +1,33 @@
 '''
-46. Permutations
-Medium
-
-Given a collection of distinct integers, return all possible permutations.
-
-Example:
-
-Input: [1,2,3]
-Output:
-[
-  [1,2,3],
-  [1,3,2],
-  [2,1,3],
-  [2,3,1],
-  [3,1,2],
-  [3,2,1]
-]
+Given an array nums of distinct integers, return all the possible permutations.
+You can return the answer in any order.
 '''
 
-def permutation(nums):
-    if len(nums) <= 0:
-        return
-    res = []
-    level = 0
-    out = []
-    visited = [False for i in range(len(nums))]
-    permuteDFS(nums, level, visited, res, out)
-    return res
 
-# https://docs.python.org/3/library/copy.html
-# shallow copy: just copy values, do not copy objects, they create bindings between a target and an object.
-# deep copy:
-# A shallow copy constructs a new compound object and then (to the extent possible)
-# inserts references into it to the objects found in the original.
-# # A deep copy constructs a new compound object and then, recursively, inserts
-# copies into it of the objects found in the original.
+
 from copy import deepcopy
-def permuteDFS(nums, level, visited, res, out):
+
+def DFS(nums, level, out, res, visited):
     if level == len(nums):
-        p = deepcopy(out)
-        res.append(p)
+        o = deepcopy(out)
+        res.append(o)
         return
     for i in range(len(nums)):
-        if visited[i]== True:
+        if visited[i] == 1:
             continue
-        visited[i] = True
+        visited[i] = 1
         out.append(nums[i])
-        permuteDFS(nums, level+1, visited, res, out)
+        DFS(nums, level + 1, out, res, visited)
         out.pop()
-        visited[i] = False
+        visited[i] = 0
+
+nums = [1,2,3]
+res = []
+out = []
+visited = [0 for i in range(len(nums))]
+level = 0
+DFS(nums, level, out, res, visited)
+
+print(res)
 
 
-
-nums = [1,2,3,4]
-print(permutation(nums))
