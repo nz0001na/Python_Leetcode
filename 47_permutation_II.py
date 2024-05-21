737fbd360ccfb81e5a016232cb7d51fcbf8c826a
NaZhang <32621160+nz0001na@users.noreply.github.com>
2024-05-20 20:25:10 -0700

    Add files via upload

---
+++ 47_permutation_II.py
@@ -0,0 +1,38 @@
+'''
+Given a collection of numbers, nums, that might contain duplicates,
+return all possible unique permutations in any order.
+'''
+
+'''
+sort nums first.
+deep copy out, to append into res.
+
+'''
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+
+        def DFS(nums, level, out, res, visited):
+            if level == len(nums):
+                o = deepcopy(out)
+                res.append(o)
+                return
+            for i in range(len(nums)):
+                if visited[i] == 1:
+                    continue
+                if i > 0 and nums[i] == nums[i - 1] and visited[i - 1] == 1:
+                    continue
+                visited[i] = 1
+                out.append(nums[i])
+                DFS(nums, level + 1, out, res, visited)
+                out.pop()
+                visited[i] = 0
+
+        res = []
+        out = []
+        nums.sort()
+        visited = [0 for i in range(len(nums))]
+        level = 0
+        DFS(nums, level, out, res, visited)
+        return res
+
+
