213b98de9ecc0f54162080778d6b5f5f25b4159e
NaZhang <32621160+nz0001na@users.noreply.github.com>
2024-05-22 16:15:28 -0700

    Add files via upload

--- 249_Group_Shifted_Strings.py
+++ 249_Group_Shifted_Strings.py
@@ -1,3 +1,44 @@
+'''
+Question: (Meduim)
+
+We can shift a string by shifting each of its letters to its successive letter.
+
+For example, "abc" can be shifted to be "bcd".
+We can keep shifting the string to form a sequence.
+
+For example, we can keep shifting "abc" to form the sequence: "abc" -> "bcd" -> ... -> "xyz".
+
+Given an array of strings strings, group all strings[i] that belong to the same shifting sequence.
+You may return the answer in any order.
+
+
+Example 1:
+Input: strings = ["abc","bcd","acef","xyz","az","ba","a","z"]
+Output: [["acef"],["a","z"],["abc","bcd","xyz"],["az","ba"]]
+
+Example 2:
+Input: strings = ["a"]
+Output: [["a"]]
+
+
+Constraints:
+
+1 <= strings.length <= 200
+1 <= strings[i].length <= 50
+strings[i] consists of lowercase English letters.
+
+'''
+
+
+'''
+Solution 1:
+use dictionary to store the result:
+        Key: pattern of string in the input array, for example, ‘#2#6#9#13#’, 
+             number between '#' means difference between two characters s[i] and s[i+1].
+        value: substring list with same pattern as shown in the key
+the values are result.
+
+'''
 class Solution:
     def groupStrings(self, strings: List[str]) -> List[List[str]]:
 
