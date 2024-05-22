b7fe3d7caa5ed94333777d2406408d1f38efd865
NaZhang <32621160+nz0001na@users.noreply.github.com>
2024-05-22 16:54:40 -0700

    Add files via upload

--- 3_longest_substring_without_repeating_characters.py
+++ 3_longest_substring_without_repeating_characters.py
@@ -1,17 +1,50 @@
 '''
-Given a string s, find the length of the longest
-substring without repeating characters.
+3. (medium)
+Given a string s, find the length of the longest substring without repeating characters.
+
+Example 1:
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+
+Example 2:
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+
+Example 3:
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+Constraints:
+
+    0 <= s.length <= 5 * 104
+    s consists of English letters, digits, symbols and spaces.
+
+
+
 '''
 
 
 '''
-create a sliding window: [left, cur-1]
-last_index show the last appearance of current char.
-length of substring: the length of sliding window.
-
-Dictionary: store last appearance of each character
-
-'space' character can not be the key in dictionary.
+Solution 1: 
+    for loop with index cur on input string:
+       create a sliding window: [left, cur-1]
+       last_index show the last appearance (index) of current char.
+       length of substring: the length of sliding window.
+    
+    Dictionary: store last appearance of each character
+    
+    if s[cur] is not in the dictionary as a key, add into the dictionary.
+    if yes, check if the index (in value) of stored key at last appearance (last_index) in s is in the sliding window.
+        if no, just update the index value of this key in dictionary.
+        if yes, get the size of the sliding window, move the left point of the sliding window to last_index+1.
+    get max length.
+    
+    note: ' ' character can not be the key in dictionary.
 
 
 '''
@@ -51,7 +84,10 @@ class Solution:
 
 
 '''
-Dictionary: store characters only appeared in the sliding window.
+Solution 2:
+
+    Dictionary: store characters only appeared in the sliding window.
+                del dict[k] if not is the sliding window.
 
 '''
 
