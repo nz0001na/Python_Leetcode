6d91e2d7950d3922dd94f147f3e1c6f737c05768
NaZhang <32621160+nz0001na@users.noreply.github.com>
2024-05-22 17:00:29 -0700

    Add files via upload

--- 300_longest_increasing_subsequence.py
+++ 300_longest_increasing_subsequence.py
@@ -3,12 +3,32 @@ Given an integer array nums, return the length of the longest strictly increasin
 subsequence
 
 
+Example 1:
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+
+Example 2:
+Input: nums = [0,1,0,3,2,3]
+Output: 4
+
+Example 3:
+Input: nums = [7,7,7,7,7,7,7]
+Output: 1
+
+Constraints:
+    1 <= nums.length <= 2500
+    -104 <= nums[i] <= 104
+
 '''
+
 
 '''
 create a array: trg, to store elements, maintain the current length that have found.
-O(n^2)
-O(n)
+
+
+space: O(n^2)
+time: O(n)
 '''
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
@@ -32,8 +52,3 @@ class Solution:
                         trg[j + 1] = cur
                         break
         return len(trg)
-
-
-'''
-
-'''
