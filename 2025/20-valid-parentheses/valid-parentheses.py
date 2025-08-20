8662a78b415cb4c76c887b23cc3c8f145ff2f4f1
NaZhang <32621160+nz0001na@users.noreply.github.com>
2025-08-20 10:09:32 -0700

    Time: 0 ms (100.00%) | Memory: 18.1 MB (9.97%) - LeetSync

--- 2025/20-valid-parentheses/valid-parentheses.py
+++ 2025/20-valid-parentheses/valid-parentheses.py
@@ -5,17 +5,17 @@ class Solution:
 
         left_list = ['(', '{', '[']
         right_list = [')', '}', ']']
-        match_dict = {')':'(', ']':'[', '}':'{'}
+        match_dict = {'(':')', '[':']', '{':'}'}
 
         stack_s = []
 
         for i in range(n):
             cur = s[i]
             if cur in left_list:
-                stack_s.append(cur)
+                stack_s.append(match_dict[cur])
             if cur in right_list:
                 if len(stack_s) <= 0: return False
-                if match_dict[cur] == stack_s[-1]:
+                if cur == stack_s[-1]:
                     stack_s.pop()
                 else:
                     return False
