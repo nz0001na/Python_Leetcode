5229658d19e3b6ffa1ffa603e40b941a7c7781f3
NaZhang <32621160+nz0001na@users.noreply.github.com>
2025-08-20 10:04:08 -0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (96.48%) - LeetSync

---
+++ 2025/20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        n = len(s)
+        if n <= 0: return None
+
+        left_list = ['(', '{', '[']
+        right_list = [')', '}', ']']
+        match_dict = {')':'(', ']':'[', '}':'{'}
+
+        stack_s = []
+
+        for i in range(n):
+            cur = s[i]
+            if cur in left_list:
+                stack_s.append(cur)
+            if cur in right_list:
+                if len(stack_s) <= 0: return False
+                if match_dict[cur] == stack_s[-1]:
+                    stack_s.pop()
+                else:
+                    return False
+        
+        if len(stack_s) == 0: 
+            return True
+        else:
+            return False
+       
+        
