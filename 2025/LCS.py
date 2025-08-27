b640ffab1c7d4e04d7b219c4d9328798c8f10cf8
NaZhang <32621160+nz0001na@users.noreply.github.com>
2025-08-27 14:49:30 -0700

    Add files via upload

--- 2025/LCS.py
+++ 2025/LCS.py
@@ -34,7 +34,34 @@ def lcs_tabulation(s1, s2):
 
 
 def lcs_tabulation_space(s1, s2):
-    
+    prev = [0 for _ in range(len(s2)+1)]
+    curr = [0 for _ in range(len(s2)+1)]
+
+    for i in range(1, len(s1)+1):
+        for j in range(1,len(s2)+1):
+            if s1[i-1] == s2[j-1]:
+                curr[j] = 1 + prev[j-1]
+            else:
+                curr[j] = max(curr[j-1], prev[j])
+        prev = curr
+        curr = [0 for _ in range(len(s2) + 1)]
+    return prev[len(s2)]
+
+
+def lcs_tabulation_space2(s1, s2):
+    curr = [0 for _ in range(len(s2)+1)]
+    for i in range(1,len(s1)+1):
+        prev = curr[0]
+        for j in range(1,len(s2)+1):
+            temp = curr[j]
+            if s1[i-1] == s2[j-1]:
+                curr[j] = 1 + prev
+            else:
+                curr[j] = max(curr[j-1], curr[j])
+            prev = temp
+    return curr[len(s2)]
+
+
 
 
 
@@ -46,6 +73,8 @@ tab = [[-1 for _ in range(cols)] for _ in range(rows) ]
 # print(lcs_native(s1, s2))
 print(lcs_memoization(s1, s2, tab))
 print(lcs_tabulation(s1, s2))
+print(lcs_tabulation_space(s1,s2))
+print(lcs_tabulation_space2(s1,s2))
 
 
 
