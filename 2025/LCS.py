a957be3687bf4f50b3721c736ebae0ce774786a5
NaZhang <32621160+nz0001na@users.noreply.github.com>
2025-08-27 12:20:41 -0700

    Add files via upload

---
+++ 2025/LCS.py
@@ -0,0 +1,51 @@
+def lcs_native(s1, s2):
+    if len(s1) == 0 or len(s2) == 0:
+        return 0
+
+    if s1[-1] == s2[-1]:
+        return 1 + lcs_native(s1[:-1], s2[:-1])
+    else:
+        return max(lcs_native(s1[:-1], s2), lcs_native(s1, s2[:-1]))
+
+
+def lcs_memoization(s1, s2, tab):
+    if len(s1) == 0 or len(s2) == 0: return 0
+    if tab[len(s1)][len(s2)] != -1: return tab[len(s1)][len(s2)]
+
+    if s1[-1] == s2[-1]:
+        tab[len(s1)][len(s2)] = 1 + lcs_memoization(s1[:-1], s2[:-1], tab)
+    else:
+        tab[len(s1)][len(s2)] = max(lcs_memoization(s1[:-1],s2,tab), lcs_memoization(s1, s2[:-1], tab))
+    return tab[len(s1)][len(s2)]
+
+
+
+def lcs_tabulation(s1, s2):
+    dp = [[0] * (len(s2)+1) for _ in range(len(s1)+1)]
+
+    for i in range(1,len(s1)+1):
+        for j in range(1, len(s2)+1):
+            if s1[i-1] == s2[j-1]:
+                dp[i][j] = 1 + dp[i-1][j-1]
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+    return dp[len(s1)][len(s2)]
+
+
+def lcs_tabulation_space(s1, s2):
+    
+
+
+
+s1 = "AGGTAB"
+s2 = "GXTXAYB"
+rows = len(s1) + 1
+cols = len(s2) + 1
+tab = [[-1 for _ in range(cols)] for _ in range(rows) ]
+# print(lcs_native(s1, s2))
+print(lcs_memoization(s1, s2, tab))
+print(lcs_tabulation(s1, s2))
+
+
+
